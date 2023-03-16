ebd058ceb1defe7a29b1fb8f55c96bfb6f1dad1b
kshirsagar1994 <127470985+kshirsagar1994@users.noreply.github.com>
2023-03-16 11:41:43 +0530

    Add files via upload

---
+++ arithmatic_op.java
@@ -0,0 +1,15 @@
+// logical operation
+
+class logical_op{
+public static void main(String args[]){
+
+int a = 10;
+int b = 5;
+int c = 20;
+
+System.out.println(a>b && a<c); 
+System.out.println(a=b || b>c); 
+System.out.println(a>c && b<a); 
+System.out.println(a<c || b>a); 
+}
+}
