105fd6873a3652d073f7c8bd125af1561d4aa1c1
kshirsagar1994 <127470985+kshirsagar1994@users.noreply.github.com>
2023-03-16 11:57:50 +0530

    Update arithmatic_op.java

--- arithmatic_op.java
+++ arithmatic_op.java
@@ -1,15 +1,14 @@
-// logical operation
+// arithmatic operation
 
-class logical_op{
+class arithmatic_op{
 public static void main(String args[]){
 
 int a = 10;
-int b = 5;
-int c = 20;
+int b = 20;
 
-System.out.println(a>b && a<c); 
-System.out.println(a=b || b>c); 
-System.out.println(a>c && b<a); 
-System.out.println(a<c || b>a); 
+System.out.println(a+b); 
+System.out.println(a-b); 
+System.out.println(a*b); 
+System.out.println(a/b); 
 }
 }
