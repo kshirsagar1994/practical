4f68b63ddde6c256014241a86c17fcf4c890c23e
kshirsagar1994 <127470985+kshirsagar1994@users.noreply.github.com>
2023-03-16 11:23:20 +0530

    Add files via upload

--- unary_op.java
+++ unary_op.java
@@ -6,14 +6,17 @@ public static void main(String args[]){
 int a = 10;
 int b = 10;
 
-System.out.println(a++); // increment by 1 postfix by 1
-System.out.println(++a); // increment by 1 prefix by 1
-System.out.println(a--); // decrement by 1 postfix by 1
-System.out.println(--a); // decrement by 1 prefix by 1 
+//System.out.println(a++); // increment by 1 postfix by 1
+//System.out.println(++a); // increment by 1 prefix by 1
+//System.out.println(a--); // decrement by 1 postfix by 1
+//System.out.println(--a); // decrement by 1 prefix by 1 
 
-System.out.println(a++ + ++b);
-System.out.println(++a + a++);
-System.out.println(b++ + ++a);
+//System.out.println(a++ + ++b); // 10+11
+//System.out.println(++a + a++); // 12+12
+//System.out.println(b++ + ++a); // 11+14
 
+System.out.println(a-- + --b); //10+9
+System.out.println(--a + a--); //8+8
+System.out.println(b-- + --a); //9+6
 }
 }
