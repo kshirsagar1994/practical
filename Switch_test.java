c772b4d0913612bfd1f9563d804eac8afe9bfdf6
kshirsagar1994 <127470985+kshirsagar1994@users.noreply.github.com>
2023-03-17 12:56:18 +0530

    Add files via upload

---
+++ Switch_test.java
@@ -0,0 +1,22 @@
+
+class Switch_test{
+public static void main(String[] args){
+
+int a = 10;
+
+switch(a) {
+
+case 0:
+System.out.println("number is 0");
+break;
+
+case 1:
+System.out.println("number is 1");
+break;
+
+default:
+System.out.println(a);
+
+}
+}
+}
