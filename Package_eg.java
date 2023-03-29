431ed18589de5a438582c07b3d64f9134ee17aa8
kshirsagar1994 <127470985+kshirsagar1994@users.noreply.github.com>
2023-03-29 23:25:54 +0530

    Add files via upload

---
+++ Package_eg.java
@@ -0,0 +1,8 @@
+package Package_test;
+
+public class Package_eg {
+	public static void main(String[] args) {
+		System.out.println("package created sucessfuly");
+	}
+
+}
---
+++ package_access.java
@@ -0,0 +1,10 @@
+package Package_test;
+
+import com.mypackage;
+
+public class package_access {
+ public static void main(String[] args) {
+	mypackage obj = new mypackage();
+	obj.display();
+}
+}
